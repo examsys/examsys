1cfa52178987522a18df41a0f08c8ae1545d29f0
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2011-09-02 15:45:56 +0000

    Remove temporary branch of trunk used when trying to get merge to main branch working
    git-svn-id: svn://suivarro/touchstone/branches/personal@659 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- nazrji/2011-09-trunk/touchstone/lang/en/touchstone/delete/do_delete_q_pointer.php
+++
@@ -1,7 +0,0 @@
-<?php
-$string['questiondeleted'] = '1Question Deleted';
-$string['msg'] = '1Question pointer successfully deleted from question paper.';
-$string['ok'] = '1OK';
-$string['updateerror'] = '1Properties Update Error';
-$string['recyclebin'] = '1Recycle Bin';
-?>
