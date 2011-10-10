50c859618b5ad1a1e7835e7eacd4629ed081a904
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2011-10-10 11:29:54 +0000

    Fix redirection URL
    git-svn-id: svn://suivarro/touchstone/branches/4.01@973 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- touchstone/users/details.php
+++ touchstone/users/details.php
@@ -259,7 +259,7 @@ a.access:hover {color:white}
   function reviewPaper(started,userid,surname,papername,log_type) {
     var winwidth = 750;
     var winheight = screen.height-80;
-    window.open("../finish.php?paperID="+papername+"&previous="+started+"&userid="+userid+"&surname="+surname+"&log_type="+log_type+"","paper","width="+winwidth+",height="+winheight+",left=30,top=20,scrollbars=yes,toolbar=no,location=no,directories=no,status=no,menubar=no,resizable");
+    window.open("../paper/finish.php?paperID="+papername+"&previous="+started+"&userid="+userid+"&surname="+surname+"&log_type="+log_type+"","paper","width="+winwidth+",height="+winheight+",left=30,top=20,scrollbars=yes,toolbar=no,location=no,directories=no,status=no,menubar=no,resizable");
   }
 
   function showTab(tabID) {
