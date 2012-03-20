ce74bae88d4573c1c076f3e9432129da3d2d1d08
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2012-03-20 16:15:26 +0000

    Add script missed in last commit
    git-svn-id: svn://suivarro/touchstone/branches/personal@2198 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

---
+++ nazrji/201203-dynamic-papers/js/jquery.dynamicpapers.js
@@ -0,0 +1,35 @@
+// This file is part of TouchStone
+//
+// TouchStone is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// TouchStone is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with TouchStone.  If not, see <http://www.gnu.org/licenses/>.
+
+$(function () {
+  // TODO: handle 'by question' display
+  $('#add-questions').click(checkObjectives);
+});
+
+function checkObjectives() {
+  var ids = '';
+  var module = $('#module').val();
+  $('.map-objective:checked').each(function() { ids += $(this).val() + ','; });
+  launchAddQuestions(module, ids.replace(/,$/, ''));
+}
+
+function launchAddQuestions(module, qns) {
+  var winH = screen.height - 80;
+  var winW = screen.width - 80;
+  var notice=window.open(cfgRootPath + "/question/add/add_questions_frame.php?module=" + module + "&questions=" + qns, "notice", "width=" + winW + ", height=" + winH + ",left=40,top=20,scrollbars=yes,toolbar=no,location=no,directories=no,status=no,menubar=no,resizable");
+  if (window.focus) {
+    notice.focus();
+  }
+}
