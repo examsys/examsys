bcf0441768e0e6573756cf0a7d080382ffe0751d
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2012-07-06 13:25:43 +0000

    Remove temp change to force dataset to 2011/12
    git-svn-id: svn://suivarro/touchstone/branches/personal@2642 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- nazrji/2012-07-cm-integration/classes/dateutils.class.php
+++ nazrji/2012-07-cm-integration/classes/dateutils.class.php
@@ -26,12 +26,9 @@
 
 Class DateUtils {
 	// Start of academic year (mm/dd)
-//  public static $academic_year_start = '07/01';
-
-  // TODO: set back to July
-  public static $academic_year_start = '09/01';
-
-  /**
+	public static $academic_year_start = '07/01';
+	
+	/**
 	 * Get the current academic year in the format 'yyyy/yy', e.g. '2010/11'
 	 * @return string
 	 */
