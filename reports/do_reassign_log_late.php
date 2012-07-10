75c29e666cfbc3dda68c914aead6841f7890e2ff
Anthony Brown <Anthony.Brown@nottingham.ac.uk>
2012-07-10 09:43:34 +0000

    Merged revision(s) 2651 from touchstone/trunk: Fix for log late error message causing issues on Live server but not dev boxes! ........ Back ported fix for above ;-)
    git-svn-id: svn://suivarro/touchstone/branches/4.2.4@2652 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- reports/do_reassign_log_late.php
+++ reports/do_reassign_log_late.php
@@ -70,7 +70,8 @@
     while($log_check->fetch()) {
       $logged_qns[$log_q_id] = $log_id;
     }
-
+    $log_check->close();
+    
     $stmt = $mysqli->prepare("SELECT q_id, mark, totalpos, user_answer, log_late.screen, ipaddress, duration, student_grade, year, updated, dismiss, attempt, option_order FROM (log_late, log_metadata) WHERE log_late.userID=log_metadata.userID AND log_late.q_paper=log_metadata.paperID AND log_late.started=log_metadata.started AND log_late.userID=? AND q_paper=? AND log_late.started=?");
     $stmt->bind_param('iis', $_POST['userID'], $_POST['paperID'], $_POST['started']);
     $stmt->execute();
@@ -90,7 +91,6 @@
         $insert->execute();
         $insert->close();
       }
-      $log_check->close();
     }
     $stmt->close();
   }
