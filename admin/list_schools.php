1f909df6ef5afeb9c55cdacd7b248e2d54e2b993
Rob Ingram <Rob.Ingram@notitngham.ac.uk>
2012-07-13 09:35:27 +0000

    Fix schools list where school of same name exists in multiple faculties
    git-svn-id: svn://suivarro/touchstone/branches/4.2.4@2654 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- admin/list_schools.php
+++ admin/list_schools.php
@@ -118,7 +118,7 @@ if ($faculties > 0) {
   $old_faculty = '';
 $id = 0;
 
-$result = $mysqli->prepare("SELECT schools.id, schools.school, faculty.name, faculty.deleted, COUNT(modules.id) FROM (schools, faculty) LEFT JOIN modules ON schools.id=modules.schoolid WHERE schools.facultyID=faculty.id AND schools.deleted IS NULL GROUP BY school ORDER BY faculty.name, school");
+$result = $mysqli->prepare("SELECT schools.id, schools.school, faculty.name, faculty.deleted, COUNT(modules.id) FROM (schools, faculty) LEFT JOIN modules ON schools.id=modules.schoolid WHERE schools.facultyID=faculty.id AND schools.deleted IS NULL GROUP BY faculty.name, school ORDER BY faculty.name, school");
 $result->execute();
 $result->bind_result($id, $school, $faculty, $faculty_deleted, $module_no);
 while ($result->fetch()) {
