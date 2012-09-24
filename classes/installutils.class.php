3707986564138c27573d37a88d15a8a92516d638
Simon Atack <Simon.Atack@notitngham.ac.uk>
2012-09-24 11:29:56 +0000

    Updated installer to support multi query on help loader
    git-svn-id: svn://suivarro/touchstone/branches/4.3@3371 8d9c46d6-3bce-41d5-b1ab-56de0fb72b2f

--- classes/installutils.class.php
+++ classes/installutils.class.php
@@ -310,7 +310,7 @@ Class InstallUtils {
       $query = file_get_contents($staff_help);
       self::$db->query("TRUNCATE staff_help");
 
-      while (self::$db->next_result()) ;
+
       self::$db->multi_query($query);
       if (self::$db->error) {
         try {
@@ -329,7 +329,9 @@ Class InstallUtils {
           if (self::$db->insert_id > 0) $ext = $ext . ' ' . self::$db->insert_id;
         }
       }
-      while (self::$db->next_result()) ;
+      while (self::$db->more_results()) {
+        self::$db->next_result();
+      }
     } else {
       self::logWarning(array('502' => $string['logwarning2']));
     }
@@ -337,7 +339,7 @@ Class InstallUtils {
     if (file_exists($student_help)) {
       $query = file_get_contents($student_help);
       self::$db->query("TRUNCATE student_help");
-      while (self::$db->next_result()) ;
+
       self::$db->multi_query($query);
       if (self::$db->error) {
         try {
@@ -351,8 +353,8 @@ Class InstallUtils {
       if (self::$db->errno != 0) {
         self::logWarning(array('503' => $string['logwarning3'] . self::$db->error));
         $ext = '';
-        while (self::$db->next_result()) {
-          if (self::$db->insert_id > 0) $ext = $ext . ' ' . self::$db->insert_id;
+        while (self::$db->more_results()) {
+          self::$db->next_result();
         }
       }
       while (self::$db->next_result()) ;
