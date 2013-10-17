2646fdac23d5adc5d7b62c815aa997b729304cd7
Simon Atack <simon.atack@nottingham.ac.uk>
2013-10-17 14:17:04 +0100

    Update Version info to 5.0.3

--- updates/version5.php
+++ updates/version5.php
@@ -29,7 +29,7 @@ require_once '../include/auth.inc';
 require_once '../classes/lang.class.php';
 require_once $cfg_web_root . 'classes/dbutils.class.php';
 
-$version = '5.0.2';
+$version = '5.0.3';
 
 set_time_limit(0);
 
