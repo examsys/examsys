f730738ab4506ec6a491317d2faf033d39c57e5f
Simon Wilkinson <simon.wilkinson@nottingham.ac.uk>
2014-04-25 17:36:14 +0100

    Fix to area question type - graphics were not uploading.

--- js/qarea.js
+++ js/qarea.js
@@ -40,7 +40,8 @@ function setUpArea(num, doorId, lang, image, config, answer, extra, colour, mode
 			this.qa_redraw_canvas;
 		}  
 		this.gen_img.onload = qa_gen_img_onload.bind(this);
-		this.gen_img.src = ''+image;
+		this.gen_img.src = ((mode == 'edit' || mode == 'correction')?'../':'')+'../media/'+image;
+		//this.gen_img.src = ''+image;
        
 		//---------- mode 
 		this.yoffset = 25; //ofset of top edge of the image
@@ -87,11 +88,7 @@ function setUpArea(num, doorId, lang, image, config, answer, extra, colour, mode
       this.qa_redraw_canvas;
 		}
 		this.menu_img.onload = menu_img_onload.bind(this);
-		if (this.qmode == 'edit') {
-			this.menu_img.src = '../../js/images/combined.png'; 
-		} else {
-			this.menu_img.src = '../js/images/combined.png'; 
-		}
+		this.menu_img.src = ((this.qmode == 'edit' || this.qmode == 'correction')?'../':'')+'../js/images/combined.png';
 	}
 }
 
