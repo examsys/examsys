e7caa798cbb2bc9cf786de6ea3d3439b994fba36
Simon Wilkinson <simon.wilkinson@nottingham.ac.uk>
2014-05-06 17:09:15 +0100

    Change to 5.1.5.

--- install/index.php
+++ install/index.php
@@ -46,7 +46,7 @@ $cfg_web_root = get_root_path() . '/';
 $cfg_root_path = ltrim(str_replace($_SERVER['DOCUMENT_ROOT'], '', $cfg_web_root), '/');
 
 require $cfg_web_root . 'classes/installutils.class.php';
-$version = '5.1.4';
+$version = '5.1.5';
 
 set_time_limit(0);
 
