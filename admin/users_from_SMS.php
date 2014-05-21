b88068cde1e7b5d2dbe9ed0273e3c343a59d3cbc
Simon Wilkinson <simon.wilkinson@nottingham.ac.uk>
2014-05-21 16:09:06 +0100

    Bug fix for #1407.

--- admin/users_from_SMS.php
+++ admin/users_from_SMS.php
@@ -59,7 +59,7 @@ $session = (isset($_GET['session']) and $_GET['session'] != '') ? $_GET['session
 $session_parts = explode('/', $session);
 
 // Do not include deleted modules or non-active modules.
-$module_data = $mysqli->prepare("SELECT modules.id, moduleid, sms FROM modules WHERE sms != '' AND mod_deleted IS NULL AND active = 1 ORDER BY moduleid LIMIT 10");
+$module_data = $mysqli->prepare("SELECT modules.id, moduleid, sms FROM modules WHERE sms != '' AND mod_deleted IS NULL AND active = 1 ORDER BY moduleid");
 $module_data->execute();
 $module_data->store_result();
 $module_data->bind_result($idMod, $module, $sms);
