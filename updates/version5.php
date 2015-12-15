089127498f7ef75cd5404e4f05114c8d1e69ea9f
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2015-12-15 14:10:25 +0000

    6.0.5.1 version bump

--- updates/version5.php
+++ updates/version5.php
@@ -32,7 +32,7 @@ require_once '../classes/dbutils.class.php';
 require_once '../classes/stringutils.class.php';
 require_once '../include/std_set_shared_functions.inc';
 
-$version = '6.0.5';
+$version = '6.0.5.1';
 $migration_path = 'version5';
 
 set_time_limit(0);
