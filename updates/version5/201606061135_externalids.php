bbf44db936ce4072ae64921d28c9942d0d01e1d3
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-09-05 10:40:45 +0100

    Merged in drjosephbaxter/rog/hotfix/upgrade_fix (pull request #581)
    fix for upgrading

--- updates/version5/201606061135_externalids.php
+++ updates/version5/201606061135_externalids.php
@@ -52,19 +52,32 @@ if ($updater_utils->check_version("6.2.0")) {
         // New type column in config type.
         $altersql = "ALTER TABLE `config` ADD COLUMN `type` VARCHAR(10) NULL AFTER `value`";
         $updater_utils->execute_query($altersql, true);
-        // Update config table with type.
-        $altersql = "UPDATE `config` SET type = '" . Config::JSON . "' WHERE setting = 'timezones' AND component = 'core'";
-        $updater_utils->execute_query($altersql, true);
-        $altersql = "UPDATE `config` SET type = '" . Config::JSON . "' WHERE setting = 'cohort_sizes' AND component = 'core'";
-        $updater_utils->execute_query($altersql, true);
-        $altersql = "UPDATE `config` SET type = '" . Config::INTEGER . "' WHERE setting = 'max_duration' AND component = 'core'";
-        $updater_utils->execute_query($altersql, true);
-        $altersql = "UPDATE `config` SET type = '" . Config::INTEGER . "' WHERE setting = 'max_sittings' AND component = 'core'";
-        $updater_utils->execute_query($altersql, true);
+        if (!$updater_utils->has_updated('rogo1559_wsconfig')) {
+            // Save json encoded list of timezones.
+            global $timezone_array;
+            $encoded_timezones = $timezone_array;
+            $encoded_cohorts = array('<whole cohort>', '0-10', '11-20', '21-30', '31-40', '41-50', '51-75', '76-100', '101-150', '151-200', '201-300',
+            '301-400', '401-500');
+            $configObject = Config::get_instance();
+            $configObject->set_db_object($mysqli);
+            $configObject->set_setting('timezones', $encoded_timezones, Config::JSON );
+            $configObject->set_setting('cohort_sizes', $encoded_cohorts, Config::JSON );
+            $configObject->set_setting('max_duration', 779, Config::INTEGER);
+            $configObject->set_setting('max_sittings', 6, Config::INTEGER);
+            $updater_utils->record_update('rogo1559_wsconfig');
+        } else {
+            // Update config table with type.
+            $altersql = "UPDATE `config` SET type = '" . Config::JSON . "' WHERE setting = 'timezones' AND component = 'core'";
+            $updater_utils->execute_query($altersql, true);
+            $altersql = "UPDATE `config` SET type = '" . Config::JSON . "' WHERE setting = 'cohort_sizes' AND component = 'core'";
+            $updater_utils->execute_query($altersql, true);
+            $altersql = "UPDATE `config` SET type = '" . Config::INTEGER . "' WHERE setting = 'max_duration' AND component = 'core'";
+            $updater_utils->execute_query($altersql, true);
+            $altersql = "UPDATE `config` SET type = '" . Config::INTEGER . "' WHERE setting = 'max_sittings' AND component = 'core'";
+            $updater_utils->execute_query($altersql, true);
+        }
         $altersql = "UPDATE `config` SET type = null WHERE component = 'plugin_ims'";
         $updater_utils->execute_query($altersql, true);
-        
-        
         $updater_utils->record_update('rogo1829_externalids');
     }
 }
