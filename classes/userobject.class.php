86fefcb7172642fabdc7442ce00a0908099f8ee9
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-11-16 14:42:42 +0000

    Merged 6.2.1 into 6.2.0 (reverted from commit 2b50babbe0a5b7925260d6ead8247eabf34f0f19)

--- classes/userobject.class.php
+++ classes/userobject.class.php
@@ -354,18 +354,7 @@ class UserObject extends RogoStaticSingleton {
     $this->staffModules = array();
 
     if ($this->has_role('Admin')) {
-      $result = $this->db->prepare("SELECT idMod, moduleID
-        FROM modules_staff, modules
-        WHERE modules_staff.idMod = modules.id and memberID = ?
-        AND modules.moduleID IS NOT NULL
-        and mod_deleted IS NULL
-        UNION
-        SELECT id, moduleID
-        FROM modules, admin_access
-        WHERE admin_access.schools_id = modules.schoolid
-        AND userID = ?
-        AND modules.moduleID IS NOT NULL
-        and mod_deleted IS NULL");
+      $result = $this->db->prepare("(SELECT idMod, moduleID FROM modules_staff, modules WHERE modules_staff.idMod = modules.id and memberID = ? AND modules.moduleID IS NOT NULL and mod_deleted IS NULL) UNION (SELECT id, moduleID FROM modules, admin_access WHERE admin_access.schools_id = modules.schoolid AND userID = ? AND modules.moduleID IS NOT NULL and mod_deleted IS NULL)");
       $result->bind_param('ii', $this->userID, $this->userID);
     } else {
       $result = $this->db->prepare("SELECT idMod, moduleID FROM modules_staff, modules WHERE modules_staff.idMod = modules.id AND memberID = ? AND modules.moduleID IS NOT NULL AND mod_deleted IS NULL ORDER BY modules.moduleID");
