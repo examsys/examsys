65aa52475ea28ac9e65f633f041229680656b7ac
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2017-06-06 14:25:52 +0100

    Merged in drjosephbaxter/rog/improvement/ROGO-2143 (pull request #717)

--- paper/finish.php
+++ paper/finish.php
@@ -188,8 +188,9 @@ if (!$is_exam_review_mode and !$is_question_preview_mode and !$is_formative_revi
   $log_metadata->set_completed_to_now();
 }
 
-// Check user has taken the paper. If they have not we need to stop them in their tracks as there is no feedback for them to review.
-if ($userObject->has_role('Student')) {
+// Check if student has taken the paper. If they have not we need to stop them in their tracks as there is no feedback for them to review.
+// Access allowed if user role is staff,student (admins should ensure staff,student users are not students in modules they are staff on)
+if ($userObject->has_role('Student') and !$userObject->has_role('Staff')) {
   if (!$userObject->user_completed_paper($paperID)) {
     $msg = sprintf($string['furtherassistance'], $configObject->get('support_email'), $configObject->get('support_email'));
     $notice->display_notice_and_exit($mysqli, $string['pagenotfound'], $msg, $string['accessdenied'], '/artwork/page_not_found.png', '#C00000', true, true);
