0d1c22ae840b72a325428229a7822eda34b627d6
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2017-06-06 14:52:03 +0100

    Merged in drjosephbaxter/rog/improvement/ROGO-2138 (pull request #722)

--- tools/tinymce/jscripts/tiny_mce/tiny_config_question_editor.js
+++ tools/tinymce/jscripts/tiny_mce/tiny_config_question_editor.js
@@ -118,9 +118,9 @@ tinyMCE.init({
     editor_selector : "mceEditorBasic",
     theme : "advanced",
     skin : "default",
-    plugins : "mee,table,visualchars,nonbreaking,xhtmlxtras,paste",
+    plugins : "paste",
     // Theme options
-    theme_advanced_buttons1 : "cut,copy,paste,|,undo",
+    theme_advanced_buttons1 : "cut,copy,paste,|,undo|,cleanup,removeformat",
     theme_advanced_buttons2 : "",
     theme_advanced_buttons3 : "",
     theme_advanced_toolbar_location : "top",
