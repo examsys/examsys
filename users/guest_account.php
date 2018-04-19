be73ba76c9f5521e13d8045cc3ab75c675073743
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2018-04-19 15:02:19 +0100

    Merged in drjosephbaxter/rog/hotfix/ROGO-2353 (pull request #882)
    ROGO-2353 config db object needs setting as not logged in
    
    Approved-by: Neill Magill <neill.magill@nottingham.ac.uk>
    Approved-by: Mike Lee <michael.leam@nottingham.ac.uk>

--- users/guest_account.php
+++ users/guest_account.php
@@ -31,7 +31,7 @@ $language = LangUtils::getLang($cfg_web_root);
 LangUtils::loadlangfile(str_replace($cfg_web_root, '', str_replace('\\', '/', ($_SERVER['SCRIPT_FILENAME']))));
 
 $mysqli = new mysqli($configObject->get('cfg_db_host'), $configObject->get('cfg_db_student_user'), $configObject->get('cfg_db_student_passwd'), $configObject->get('cfg_db_database'));
-
+$configObject->set_db_object($mysqli);
 // Check client address of current user is in a lab.
 $address = NetworkUtils::get_client_address();
 $labfactory = new LabFactory($mysqli);
