53c65aab5705fb4ed8ff2c97be845bbc1fd58c1a
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2018-07-30 10:41:20 +0100

    ROGO-2389 do not close database connection

--- question/add/add_questions_frame.php
+++ question/add/add_questions_frame.php
@@ -23,7 +23,6 @@
 */
 
 require '../../include/staff_auth.inc';
-$mysqli->close();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
