d8b2c7eb770b5b626c6d4ae982a7ae5e0dbe67fc
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2020-02-19 15:17:48 +0000

    ROGO-2746 hotspot correction fixes

--- lang/en/question/edit/hotspot_correct.php
+++ lang/en/question/edit/hotspot_correct.php
@@ -4,3 +4,6 @@ $string['correctAnswers'] = 'Correct Answers';
 $string['incorrectAnswers'] = 'Incorrect Answers';
 $string['errorcanvas'] = 'Canvas not supported';
 $string['errorimageshotspot'] = 'Hotspot question cannot be displayed because some images were not loaded.';
+$string['hotspotcorrection'] = 'The green dots show students answers which will be marked as correct if saved.';
+$string['image'] = 'Image';
+$string['correctionmode'] = 'Correction mode';
