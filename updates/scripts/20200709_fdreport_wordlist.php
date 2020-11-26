b6cfebf33859d68e805fa3fda28762e2a78b2348
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2020-11-26 17:09:38 +0000

    remove from patch

--- updates/scripts/20200709_fdreport_wordlist.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!$updater_utils->has_updated('lancaster/feature/GJLU-245')) {
-  $configObject->set_setting('rpt_fd_show_wordlist', 1, Config::BOOLEAN);
-  $updater_utils->execute_query($sql, false);
-
-  $updater_utils->record_update('lancaster/feature/GJLU-245');
-}
