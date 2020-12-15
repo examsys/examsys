791eaf74da9d4ac321e82d77c6ba26979cb74f78
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2020-12-15 11:56:51 +0000

    removing ROGO-2855  from patch

--- updates/scripts/20200709_fdreport_wordlist.php
+++ updates/scripts/20200709_fdreport_wordlist.php
@@ -16,6 +16,8 @@
 // along with Rogō.  If not, see <http://www.gnu.org/licenses/>.
 
 if (!$updater_utils->has_updated('lancaster/feature/GJLU-245')) {
-    $configObject->set_setting('rpt_fd_show_wordlist', 1, Config::BOOLEAN);
-    $updater_utils->record_update('lancaster/feature/GJLU-245');
+  $configObject->set_setting('rpt_fd_show_wordlist', 1, Config::BOOLEAN);
+  $updater_utils->execute_query($sql, false);
+
+  $updater_utils->record_update('lancaster/feature/GJLU-245');
 }
