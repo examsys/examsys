1821f59ef7fd0c53b3b5d7f35a6f280272e535ba
Neill Magill <neill.magill@nottingham.ac.uk>
2024-09-05 10:49:41 +0100

    EXAMSYS-3492 Fixes for pull request
    I will rebase them into the main normalisation commit before merging it.
    
    They are staying seperate for now so you can more easily see what has changed.

--- reports/blank_remark.php
+++ reports/blank_remark.php
@@ -65,7 +65,7 @@ if ($paper_type == \assessment::TYPE_FORMATIVE) {
                     log1 l, log_metadata lm 
                 WHERE 
                     l.metadataID = lm.id AND l.q_id = ? AND lm.paperID = ?  
-                    AND DATE_ADD(lm.started, INTERVAL $progress_time_int MINUTE)d >= ? AND lm.started <= ? 
+                    AND DATE_ADD(lm.started, INTERVAL $progress_time_int MINUTE) >= ? AND lm.started <= ? 
                     AND student_grade NOT LIKE 'university%' AND student_grade NOT LIKE 'Staff%' 
                     AND student_grade NOT LIKE '%nhs%'
             )";
