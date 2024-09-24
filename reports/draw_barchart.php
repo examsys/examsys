aac4238c9a1379455adddad7c47cfc9da4fc6a89
Neill Magill <neill.magill@nottingham.ac.uk>
2024-09-24 14:17:04 +0100

    EXAMSYS-3459 Stop implicit conversions in bar chart
    Before this change it was possible for floating point numbers to be passed as a coordinate into an image function that only accepts an integer.
    
    This change ensures that all numbers should now be integers all the time.

--- reports/draw_barchart.php
+++ reports/draw_barchart.php
@@ -52,7 +52,8 @@ $intervals = array(0, 1, 1, 1, 1, 1, 2, 2, 2, 3, 2, 2, 3, 3, 3, 3, 4, 3, 3, 3, 4
 //small ticks
 $gap1 = ($g_x2 - $g_x1) / $total_possible_mark;
 for ($label = 0; $label <= $total_possible_mark; $label++) {
-    ImageLine($Image, $g_x1 + $gap1 * $label, $g_y1, $g_x1 + $gap1 * $label, $g_y1 - 2, $dkgrey);
+    $line_x = floor($g_x1 + $gap1 * $label);
+    ImageLine($Image, $line_x, $g_y1, $line_x, $g_y1 - 2, $dkgrey);
 }
 
 //large ticks and halflines
@@ -64,7 +65,7 @@ $step = $total_possible_mark / $this_interval;
 $gap2 = ($g_x2 - $g_x1) / $step;
 
 for ($label = 0; $label <= $step; $label++) {
-    $this_x = $g_x1 + $gap2 * $label;
+    $this_x = floor($g_x1 + $gap2 * $label);
     $this_label = $label * $this_interval;
 
     if ($total_possible_mark - $this_label < $this_interval) {
@@ -82,27 +83,30 @@ for ($label = 0; $label <= $step; $label++) {
 
 //bars
 $gap3 = 3;
-$gap4 = ($g_y2 - $g_y1 + $gap3) / 2;
+// The value is used as an integer co-ordinate we want to avoid implicit conversions.
+$gap4 = floor(($g_y2 - $g_y1 + $gap3) / 2);
 
 //student mark bar
 $student_mark = round($student_mark, 1);
-ImageFilledRectangle($Image, $g_x1, $g_y1 + $gap3, $g_x1 + $gap1 * $student_mark, $g_y1 + $gap4 - $gap3, $amber);
-ImageRectangle($Image, $g_x1, $g_y1 + $gap3, $g_x1 + $gap1 * $student_mark, $g_y1 + $gap4 - $gap3, $black);
+$student_x = floor($g_x1 + $gap1 * $student_mark);
+ImageFilledRectangle($Image, $g_x1, $g_y1 + $gap3, $student_x, $g_y1 + $gap4 - $gap3, $amber);
+ImageRectangle($Image, $g_x1, $g_y1 + $gap3, $student_x, $g_y1 + $gap4 - $gap3, $black);
 if (mb_strlen($student_mark) > 2) {
-    imagettftext($Image, 10, 0, $g_x1 + ($gap1 * $student_mark) - 20, $g_y1 + 15, $color, $bold_font, $student_mark);
+    imagettftext($Image, 10, 0, $student_x - 20, $g_y1 + 15, $color, $bold_font, $student_mark);
 } elseif ($student_mark > 0) {
-    imagettftext($Image, 10, 0, $g_x1 + ($gap1 * $student_mark) - 10, $g_y1 + 15, $color, $bold_font, $student_mark);
+    imagettftext($Image, 10, 0, $student_x - 10, $g_y1 + 15, $color, $bold_font, $student_mark);
 }
 
   //median mark bar
 $median = round($median, 1);
-ImageFilledRectangle($Image, $g_x1, $g_y1 + $gap4, $g_x1 + $gap1 * $median, $g_y2 - $gap3, $color);
-ImageRectangle($Image, $g_x1, $g_y1 + $gap4, $g_x1 + $gap1 * $median, $g_y2 - $gap3, $black);
+$mark_x = floor($g_x1 + $gap1 * $median);
+ImageFilledRectangle($Image, $g_x1, $g_y1 + $gap4, $mark_x, $g_y2 - $gap3, $color);
+ImageRectangle($Image, $g_x1, $g_y1 + $gap4, $mark_x, $g_y2 - $gap3, $black);
 
 if (mb_strlen($median) > 2) {
-    imagettftext($Image, 10, 0, $g_x1 + ($gap1 * $median) - 19, $g_y1 + 37, $black, $font, $median);
+    imagettftext($Image, 10, 0, $mark_x - 19, $g_y1 + 37, $black, $font, $median);
 } elseif ($median > 0) {
-    imagettftext($Image, 10, 0, $g_x1 + ($gap1 * $median) - 9, $g_y1 + 37, $black, $font, $median);
+    imagettftext($Image, 10, 0, $mark_x - 9, $g_y1 + 37, $black, $font, $median);
 }
 
 //axis
